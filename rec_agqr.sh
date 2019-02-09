1867ad2b3f26f7203ad778e38dcdd4ecdb05e153
tommy <31590501+tommy-git@users.noreply.github.com>
2019-02-09 11:10:08 +0900

    create recording script
    intrim version cotrol with cron

---
+++ rec_agqr.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+PATH=/usr/local/bin:$PATH
+
+usage() {
+    echo "Usage: ${command} ${time} ${download_path}"
+    echo "time: seconds"
+    echo "download_path: path to save file"
+}
+
+if [ $# -ne 2 ]; then
+    usage
+fi
+
+CONNECT_URL='rtmp://fms-base1.mitene.ad.jp/agqr/aandg22'
+TIME=${1}
+currenttime=`date +%Y%m%d-%H%M%S`
+FILENAME=${2}/${currenttime}
+
+rtmpdump -r ${CONNECT_URL} --live -B ${TIME} -o ${FILENAME}.flv
+if [ $? -ne 0  ]; then
+    echo "Failed to record"
+else
+    ffmpeg -v quiet -y -i ${FILENAME}.flv -acodec libmp3lame -aq 2 ${FILENAME}.mp3 -ss 00:00:00.001
+    if [ $? -ne 0 ]; then
+	echo "Failed to convert record data"
+    else
+	rm -f ${FILENAME}.flv
+    fi
+fi
