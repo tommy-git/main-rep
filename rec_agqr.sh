82fb30ed4c059499e61d1d7a5a41d42b400a10cb
tommy <31590501+tommy-git@users.noreply.github.com>
2020-11-26 00:25:08 +0900

    updated due to system changes

--- rec_agqr.sh
+++ rec_agqr.sh
@@ -12,19 +12,16 @@ if [ $# -ne 2 ]; then
     usage
 fi
 
-CONNECT_URL=${find with your own}
+CONNECT_URL="https://www.uniqueradio.jp/agplayer5/hls/mbr-ff.m3u8"
+#CONNECT_URL="rtmp://fms-base2.mitene.ad.jp/agqr/aandg1"
+#CONNECT_URL="rtmp://fms-base1.mitene.ad.jp/agqr/aandg22" # till 202010
 TIME=${1}
 currenttime=`date +%Y%m%d-%H%M%S`
 FILENAME=${2}/${currenttime}
+TEMPFILE=${2}/recFile.mp4
 
-rtmpdump -r ${CONNECT_URL} --live -B ${TIME} -o ${FILENAME}.flv > /dev/null
-if [ $? -ne 0  ]; then
-    echo "Failed to record"
-else
-    ffmpeg -v quiet -y -i ${FILENAME}.flv -acodec libmp3lame -aq 2 ${FILENAME}.mp3 -ss 00:00:00.001 > /dev/null
-    if [ $? -ne 0 ]; then
-	echo "Failed to convert record data"
-    else
-	rm -f ${FILENAME}.flv
-    fi
+ffmpeg -i https://www.uniqueradio.jp/agplayer5/hls/mbr-ff.m3u8 -movflags faststart -t 120 -c copy ${TEMPFILE}.mp4
+ffmpeg -i Downloads/recFile.mp4 -acodec libmp3lame -ab 256k ${FILENAME}.mp3
+if [ $? = 0 ]
+	rm -f ${TEMPFILE}.mp4
 fi
