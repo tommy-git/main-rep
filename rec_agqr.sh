cd1538c9f53dd99ba2f237cfaebbe75280a52bab
tommy <31590501+tommy-git@users.noreply.github.com>
2020-12-07 07:43:22 +0900

    fixed

--- rec_agqr.sh
+++ rec_agqr.sh
@@ -13,15 +13,13 @@ if [ $# -ne 2 ]; then
 fi
 
 CONNECT_URL="https://www.uniqueradio.jp/agplayer5/hls/mbr-ff.m3u8"
-#CONNECT_URL="rtmp://fms-base2.mitene.ad.jp/agqr/aandg1"
-#CONNECT_URL="rtmp://fms-base1.mitene.ad.jp/agqr/aandg22" # till 202010
 TIME=${1}
 currenttime=`date +%Y%m%d-%H%M%S`
 FILENAME=${2}/${currenttime}
 TEMPFILE=${2}/recFile.mp4
 
-ffmpeg -i https://www.uniqueradio.jp/agplayer5/hls/mbr-ff.m3u8 -movflags faststart -t 120 -c copy ${TEMPFILE}.mp4
-ffmpeg -i Downloads/recFile.mp4 -acodec libmp3lame -ab 256k ${FILENAME}.mp3
-if [ $? = 0 ]
-	rm -f ${TEMPFILE}.mp4
+ffmpeg -i ${CONNECT_URL} -movflags faststart -t ${TIME} -c copy ${TEMPFILE} -loglevel fatal
+ffmpeg -i ${TEMPFILE} -acodec libmp3lame -ab 256k ${FILENAME}.mp3 -loglevel fatal
+if [ $? = 0 ]; then
+	rm -f ${TEMPFILE}
 fi
